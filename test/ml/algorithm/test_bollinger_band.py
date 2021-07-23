8a1723c48ee3bf836c915691e9a11e7f32d7a2da
mattya_monaca <mattya_monaca@gmail.com>
2021-07-23 14:58:46 +0900

    add bollingerband test

--- test/ml/algorithm/test_bollinger_band.py
+++ test/ml/algorithm/test_bollinger_band.py
@@ -1,2 +1,7 @@
 import pytest
-from scripts.ml import entry_point
+from scripts.ml.entry_point import run
+
+def test_bollinger_band():
+    alg = "bollinger_band"
+    ymd = "2021-07-01"
+    run(alg, ymd, mode="test")
