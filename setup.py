fbe5e7eef7e039d1e41478b5e404743668c0daa1
mattya_monaca <mattya_monaca@gmail.com>
2021-07-26 11:25:26 +0900

    fix setuptool

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name="stockscope",
     version="0.0.1",
-    packages=find_packages(where="scripts"),
-    package_dir={"": "scripts"}
+    packages=[
+        "scripts", 
+        "scripts/ml", 
+        "scripts/ml/preprocess", 
+        "scripts/ml/config", 
+        "scripts/ml/util", 
+        "scripts/ml/model",
+        "scripts/ml/model/ml"
+    ]
 )
